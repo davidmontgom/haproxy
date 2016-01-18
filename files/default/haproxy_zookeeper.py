d307306155a4d4a3381434d5d07415f99a1f489d
David Montgomery <davidmontgomery@gmail.com>
2016-01-19 07:47:47 +0800

    f

--- files/default/haproxy_zookeeper.py
+++ files/default/haproxy_zookeeper.py
@@ -6,6 +6,7 @@ import os
 import sys
 import psutil
 import base64 
+import string
 from pprint import pprint
 import logging #https://kazoo.readthedocs.org/en/latest/basic_usage.html
 logging.basicConfig()
@@ -100,19 +101,45 @@ def create_cgf(path,addresses,server_type,meta):
         proxy_port = meta['proxy_port']
     else:
         proxy_port = meta['remote_port']
+    remote_port = meta['remote_port']
 
     temp = []
     for index,ip in enumerate(list(addresses)):
-        temp.append('server %s-%s %s:%s check' % (server_type,index+1,ip,proxy_port))
+        temp.append('server %s-%s %s:%s check' % (server_type,index+1,ip,remote_port))
+        
     temp = '\n'.join(temp)
-    temp_ha = """
-    listen %s  %s:%s
-    mode %s
-    option tcpka
-    option tcplog
-    balance roundrobin
-    %s
-    """ % (server_type,meta['host'],meta['remote_port'],mode,temp)
+#     temp_ha = """
+#     listen %s  %s:%s
+#     mode %s
+#     option tcpka
+#     option tcplog
+#     balance roundrobin
+#     %s
+#     """ % (server_type,meta['host'],meta['remote_port'],mode,temp)
+    
+
+    replace_values = { 'server_type':server_type,'mode':mode,'server_list':temp,'proxy_port':proxy_port,'remote_port':remote_port}
+    t = string.Template("""
+    frontend ${server_type}_front
+       bind 127.0.0.1:${proxy_port}
+       mode $mode
+       option ${mode}log
+       default_backend ${server_type}_backend
+    
+    backend ${server_type}_backend
+       mode $mode
+       option ${mode}log
+       balance roundrobin
+       $server_list
+    """)
+    temp_ha = t.substitute(replace_values)
+    
+    
+    
+    
+    
+    
+    
     
     ip_encode = get_ip_encode(addresses)
     os.system('rm /etc/haproxy/conf.d/%s*.cfg' % (server_type))
