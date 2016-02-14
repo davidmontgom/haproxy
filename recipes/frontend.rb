d6720ee406c933cb1c8826a3ca281b9b83c56e97
David Montgomery <davidmontgomery@gmail.com>
2016-02-14 19:40:04 +0800

    update

--- recipes/frontend.rb
+++ recipes/frontend.rb
@@ -1,13 +1,33 @@
+server_type = node.name.split('-')[0]
+slug = node.name.split('-')[1] 
+datacenter = node.name.split('-')[2]
+environment = node.name.split('-')[3]
+location = node.name.split('-')[4]
+cluster_slug = File.read("/var/cluster_slug.txt")
+cluster_slug = cluster_slug.gsub(/\n/, "") 
 
 
-bash "ufw_enable_80" do
-  user "root"
-  cwd "#{Chef::Config[:file_cache_path]}"
-  code <<-EOH
-    sudo ufw allow 80/tcp
-    touch #{Chef::Config[:file_cache_path]}/ufw_enable_80
-  EOH
-  action :run
-  not_if {File.exists?("#{Chef::Config[:file_cache_path]}/ufw_enable_80")}
+data_bag("server_data_bag")
+haproxy_server = data_bag_item("server_data_bag", "haproxy")
+
+public_ports = haproxy_server_server[datacenter][environment][location][cluster_slug]['public_ports']
+
+
+public_ports.each do |port|
+  
+  bash "open_public_ports" do
+      user "root"
+      code <<-EOH
+        
+        /sbin/iptables -I INPUT -i eth0 -p tcp --dport #{port} -m state --state NEW,ESTABLISHED -j ACCEPT
+        /sbin/iptables -I INPUT -i eth0 -p tcp --sport #{port} -m state --state ESTABLISHED -j ACCEPT
+        /etc/init.d/iptables-persistent save
+        touch /var/chef/cache/iptables_#{port}.lock
+      EOH
+      action :run
+      not_if {File.exists?("/var/chef/cache/iptables_#{port}.lock")}
+  end
+
 end
+
 
