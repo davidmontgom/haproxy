ce653f646f9bcc711943fc4d448baedeb4be290a
David Montgomery <davidmontgomery@gmail.com>
2016-02-15 22:40:04 +0800

    update

--- recipes/frontend.rb
+++ recipes/frontend.rb
@@ -19,7 +19,7 @@ public_ports.each do |port|
       code <<-EOH
         
         /sbin/iptables -I INPUT -i eth0 -p tcp --dport #{port} -m state --state NEW,ESTABLISHED -j ACCEPT
-        /sbin/iptables -I INPUT -i eth0 -p tcp --sport #{port} -m state --state ESTABLISHED -j ACCEPT
+        /sbin/iptables -I OUTPUT -o eth0 -p tcp --sport #{port} -m state --state ESTABLISHED -j ACCEPT
         /etc/init.d/iptables-persistent save
         
         touch /var/chef/cache/iptables_#{port}.lock
