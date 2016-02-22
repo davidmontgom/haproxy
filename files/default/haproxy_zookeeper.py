047856fa593cd08796aa0908beed6582270975ed
David Montgomery <davidmontgomery@gmail.com>
2016-02-23 06:19:33 +0800

    update

--- files/default/haproxy_zookeeper.py
+++ files/default/haproxy_zookeeper.py
@@ -8,50 +8,48 @@ import os
 import sys
 import psutil
 import base64 
+import hashlib
 import string
 from pprint import pprint
 import logging #https://kazoo.readthedocs.org/en/latest/basic_usage.html
 logging.basicConfig()
+from bootops.classes import getparms
 
-"""
-0) load zookeeper from file written by chef
-1) get ip address from file
-2) register server
-3) If change in servers then rerun chef
+running_in_pydev = 'PYDEV_CONSOLE_ENCODING' in os.environ
+if running_in_pydev==False:
+    if os.path.isfile('/var/cluster_slug.txt'):
+        cluster_slug = open("/var/cluster_slug.txt").readlines()[0].strip()
+    else:
+        cluster_slug = "nocluster"
+else:
+    environment = "development"
+    location = "east"
+    datacenter = "aws"
+    slug = "seo"
+    zk_host_str = "ec2-54-196-252-196.compute-1.amazonaws.com:2181"
+    cluster_slug = "nocluster"
+    settings_path = "/home/ubuntu/workspace/seo-settings"
+    this_server_type = "dccom"
+       
+parms = getparms.get_parms(slug='feed')
+environment = parms['environment']
+location = parms['location']
+datacenter = parms['datacenter']
+slug = parms['slug']['slug']
+this_server_type = parms['server_type']
+settings_path = parms['settings_path']
 
-How to fix sharding 
 
-"haproxy":{"elasticsearch":"127.0.0.1:9200",
-               "sentinal":"127.0.0.1:26379",
-               "kafka":"127.0.0.1:9092",
-               "zookeeper":"127.0.0.1:2181",
-               "druidbroker":"127.0.0.1:8082"   
-    },
-
-1)     
-"haproxy":[{"server_type": "elasticsearch",
-            "address":"127.0.0.1",
-            "port":"9200"
-            "shard":"True"}
-}\
-
-2) Or for every server type, if not found then check if beciase of shard
-   then issue solved.  This is prob the easiest
+haproxy_server = parms['haproxy']['haproxy']
+if parms['haproxy']['haproxy'].has_key(cluster_slug):
+    if parms['haproxy']['haproxy'][cluster_slug].has_key('emperor'):
+        emperor = parms['haproxy']['haproxy'][cluster_slug]['emperor']
+    else:
+        emperor = False
+else:
+    emperor = False
   
 
-
-
-"""
-
-# with open('ha_services.json') as data_file:    
-#     service_hash = json.load(data_file)
-#zk_host_list = '107.170.219.233'
-
-
-"""
-haproxy name server_type-cluster_slug
-
-"""
 
 zk_chksum_init = hashlib.md5(open('/var/zookeeper_hosts.json', 'rb').read()).hexdigest()
 
@@ -71,42 +69,6 @@ def get_zk_host_str(zk_host_list):
     zk_host_str = ','.join(zk_host_list)
     return zk_host_str
 
-
-running_in_pydev = 'PYDEV_CONSOLE_ENCODING' in os.environ
-if running_in_pydev==False:
-    SETTINGS_FILE='/root/.bootops.yaml'
-    from yaml import load, dump
-    from yaml import Loader, Dumper
-    f = open(SETTINGS_FILE)
-    parms = load(f, Loader=Loader)
-    f.close()
-    
-    environment = parms['environment']
-    location = parms['location']
-    datacenter = parms['datacenter']
-    slug = parms['slug']
-    this_server_type = parms['server_type']
-    settings_path = parms['settings_path']
-    if os.path.isfile('/var/cluster_slug.txt'):
-        cluster_slug = open("/var/cluster_slug.txt").readlines()[0].strip()
-    else:
-        cluster_slug = "nocluster"
-        
-#     zk_host_list = open('/var/zookeeper_hosts.json').readlines()[0]
-#     zk_host_list = zk_host_list.split(',')
-#     for i in xrange(len(zk_host_list)):
-#         zk_host_list[i]=zk_host_list[i]+':2181' 
-#     zk_host_str = ','.join(zk_host_list)
-else:
-    environment = "development"
-    location = "ny"
-    datacenter = "do"
-    slug = "forex"
-    zk_host_str = "1-zookeeper-forex-do-development-ny.forexhui.com:2181"
-    cluster_slug = "pixel"
-    settings_path = "/home/ubuntu/workspace/forex-settings"
-    this_server_type = "haproxy"
-
 def get_zk_conn():
     zk_host_list = get_zk_host_list()
     if zk_host_list:
@@ -121,6 +83,152 @@ def get_zk_conn():
 zk = get_zk_conn()
 
 
+
+def get_emperor_hash():
+    
+    emperor_hash = {}
+    for server_type, meta in parms.iteritems():
+        if isinstance(meta, dict):
+            if meta.has_key(datacenter):
+                if meta[datacenter].has_key(environment):
+                    if meta[datacenter][environment].has_key(location):
+                        #print meta[datacenter][environment][location].keys()
+                        for cs in meta[datacenter][environment][location].keys():
+                            if isinstance(meta[datacenter][environment][location][cs], dict):
+                                if meta[datacenter][environment][location][cs].has_key("emperor_domain"):
+                                    temp = '%s-%s' % (server_type,cs)
+                    
+                                    emperor_hash[temp] = {}
+                                    if cs=='nocluster':
+                                        base = "%s-%s-%s-%s-%s" % (server_type,slug,datacenter,environment,location)
+                                    else:
+                                        base = "%s-%s-%s-%s-%s-%s" % (server_type,slug,datacenter,environment,location,cs)
+                                    emperor_hash[temp]['base']=base
+                                    emperor_hash[temp]['domain']=meta[datacenter][environment][location][cs]["emperor_domain"]
+               
+    return emperor_hash
+
+def create_frontend(emperor_hash):
+    
+    server_type_app_hash = {}
+    
+    acl_string = ''
+    for server_type,meta in emperor_hash.iteritems():
+        print server_type,meta['domain']
+        for domain in meta['domain']:
+            acl_string = acl_string + 'acl %s hdr(host) -i %s' % (server_type, domain) + '\n'
+        
+    backend_string = ''
+    for server_type,meta in emperor_hash.iteritems():
+        backend_string = backend_string + 'use_backend %s_backend if %s' % (server_type,server_type) + '\n'
+       
+    t = """
+    frontend public
+    
+    # Listen on port 80
+    bind *:80
+    
+    mode http
+
+    # Define ACLs for each domain
+    %s
+    
+    # Figure out which backend (= VM) to use
+    %s
+    
+    """ % (acl_string,backend_string)
+
+    return t
+
+def create_backend(emperor_hash,base_ip_hash):
+    remote_port = 80
+    proxy_port = 80
+    mode = 'http'
+    temp_ha = []
+    for server_type,meta in emperor_hash.iteritems():
+
+        base = meta['base']
+        server_list = base_ip_hash[base] 
+        
+        temp = []
+        for index,ip in enumerate(list(server_list)):
+            temp.append('server %s-%s %s:%s check cookie s%s' % (server_type,index+1,ip,remote_port,index+1))   
+        temp = '\n'.join(temp)
+        
+        replace_values = { 'server_type':server_type,'mode':mode,'server_list':temp,'proxy_port':proxy_port,'remote_port':remote_port}
+        t = string.Template("""
+        
+        backend ${server_type}_backend
+           option httpclose
+           option forwardfor
+           http-request set-header X-Forwarded-Port %[dst_port]
+           http-request add-header X-Forwarded-Proto https if { ssl_fc }
+           
+           cookie SERVERID insert indirect nocache
+           mode $mode
+           option ${mode}log
+           balance roundrobin
+           $server_list
+        """)
+        temp_ha.append(t.substitute(replace_values))
+    temp_ha = '\n'.join(temp_ha)
+
+    return temp_ha
+    
+def my_func(event):
+    # check to see what the children are now
+    path = event.path
+    addresses = zk.get_children(event.path)
+    create_cgf(path,addresses)  
+
+def get_ip_encode(children):
+
+    ip_encode = ''.join(list(children))
+    ip_encode = base64.b64encode(ip_encode)
+    return ip_encode
+
+def emperor_mode(parms):
+    
+    environment = parms['environment']
+    location = parms['location']
+    datacenter = parms['datacenter']
+    slug = parms['slug']['slug']
+    
+    emperor_hash = get_emperor_hash()
+    base_ip_hash = {}
+    for key,value in emperor_hash.iteritems():
+        path = '/%s/' % value['base']
+        exists = zk.exists(path)
+        if exists:
+            children = zk.get_children(path, watch=my_func)
+            ip_encode = get_ip_encode(children)
+            base_ip_hash[value['base']]=list(children)
+    
+    #base_ip_hash['dccom-seo-aws-development-east']=['127.0.0.1']
+    pprint(emperor_hash)
+    haproxy_frontend = create_frontend(emperor_hash)
+    haproxy_backend = create_backend(emperor_hash,base_ip_hash)
+    
+    ha_proxy_config = '%s\n%s' % (haproxy_frontend,haproxy_backend)
+    haproxy_encode = hashlib.md5(ha_proxy_config).hexdigest()
+    
+    reload = False
+    if  os.path.isfile('/etc/haproxy/conf.d/ha_enocde_%s' % haproxy_encode)==False:
+        os.system('rm /etc/haproxy/conf.d/ha_enocde_*')
+        os.system('touch /etc/haproxy/conf.d/ha_enocde_%s' % haproxy_encode)
+        f = open('/etc/haproxy/conf.d/emperor.cfg','w')
+        f.write(ha_proxy_config)
+        f.close()
+        reload = True
+    
+    if reload:
+        os.system('rm /etc/haproxy/haproxy.cfg')
+        os.system("cat /etc/haproxy/haproxy.cfg.orig /etc/haproxy/conf.d/emperor.cfg >> /etc/haproxy/haproxy.cfg")
+        os.system('/usr/sbin/service haproxy reload')
+        print "reloading haproxy"
+        sys.stdout.flush()
+        sys.stderr.flush()
+    
 def create_cgf(path,addresses,server_type,meta):
     
 
@@ -246,12 +354,6 @@ def create_cgf(path,addresses,server_type,meta):
     sys.stdout.flush()
     sys.stderr.flush()
     
-def my_func(event):
-    # check to see what the children are now
-    path = event.path
-    addresses = zk.get_children(event.path)
-    create_cgf(path,addresses)
-
 def get_service_hash(settings_path,server_type):
     fn = "%s/server_data_bag/%s.json" % (settings_path,server_type)
     with open(fn) as data_file:    
@@ -305,41 +407,40 @@ def get_service_hash(settings_path,server_type):
         
     return service_hash, zookeeper_path_list
 
-def get_ip_encode(children):
-    
-    ip_encode = ''.join(list(children))
-    ip_encode = base64.b64encode(ip_encode)
-    return ip_encode
- 
 while True:
-    service_hash, zookeeper_path_list = get_service_hash(settings_path,this_server_type)
-    print 'mymeta',service_hash, zookeeper_path_list
     
-    zk_chksum = hashlib.md5(open('/var/zookeeper_hosts.json', 'rb').read()).hexdigest()
-    if zk_chksum!=zk_chksum:
-        zk = get_zk_conn()
+    parms = getparms.get_parms(slug='seo')
+    if emperor:
+        emperor_mode(parms)
+    else:
+        service_hash, zookeeper_path_list = get_service_hash(settings_path,this_server_type)
+        print 'mymeta',service_hash, zookeeper_path_list
         
-    
-    for server_type,meta in service_hash.iteritems():
-        
-        path = meta['path']
-
-        try:
-            exists = zk.exists(path)
-        except KazooException:
-            exists = None
+        zk_chksum = hashlib.md5(open('/var/zookeeper_hosts.json', 'rb').read()).hexdigest()
+        if zk_chksum!=zk_chksum:
             zk = get_zk_conn()
-        #print path,exists
-        if exists:
-            children = zk.get_children(path, watch=my_func)
-            #print children
-            ip_encode = get_ip_encode(children)
-               
-            if os.path.isfile('/etc/haproxy/conf.d/%s-%s.cfg' % (server_type,ip_encode))==False:
-                create_cgf(path,list(children),server_type,meta)
-   
- 
+            
+        for server_type,meta in service_hash.iteritems():
+            
+            path = meta['path']
+            try:
+                exists = zk.exists(path)
+            except KazooException:
+                exists = None
+                zk = get_zk_conn()
+            print path,exists
+
+            if exists:
+                children = zk.get_children(path, watch=my_func)
+                #print children
+                ip_encode = get_ip_encode(children)
+                    
+                if os.path.isfile('/etc/haproxy/conf.d/%s-%s.cfg' % (server_type,ip_encode))==False:
+                    create_cgf(path,list(children),server_type,meta)
+    
     sys.stdout.flush()
     sys.stderr.flush()
     print '-'*20
     time.sleep(1)
+
+ 
